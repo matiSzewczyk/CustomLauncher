2f9a8c8dae3864b4548bfda42bd3dcae21cab2aa
matiSzewczyk <mateusz.szewczyk@proget.pl>
2025-03-05 21:25:31 +0100

    Disable back button and make wallpaper visible in background

--- app/src/main/java/com/matis/customlauncher/ui/main/MainActivity.kt
+++ app/src/main/java/com/matis/customlauncher/ui/main/MainActivity.kt
@@ -5,11 +5,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.matis.customlauncher.device.PackageManagerWrapper
 import com.matis.customlauncher.navigation.MainNavHost
 import com.matis.customlauncher.ui.rememberAppState
@@ -31,7 +31,7 @@ class MainActivity : ComponentActivity() {
             val appState = rememberAppState(packageManagerWrapper, rememberCoroutineScope())
 
             CustomLauncherTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
+                Scaffold(modifier = Modifier.fillMaxSize(), containerColor = Color.Transparent) {
                     MainNavHost(appState = appState)
                 }
             }
--- app/src/main/java/com/matis/customlauncher/ui/main/MainScreen.kt
+++ app/src/main/java/com/matis/customlauncher/ui/main/MainScreen.kt
@@ -2,6 +2,8 @@ package com.matis.customlauncher.ui.main
 
 import android.content.Intent
 import android.provider.Settings
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,19 +14,26 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.matis.customlauncher.ui.AppState
 import com.matis.customlauncher.ui.theme.CustomLauncherTheme
 
 @Composable
-fun MainScreen(appState: AppState) {
+fun MainScreen(
+    appState: AppState,
+    viewModel: MainViewModel = hiltViewModel()
+) {
     val hasDefaultLauncherPermission by appState.hasDefaultLauncherPermission.collectAsStateWithLifecycle()
     val context = LocalContext.current
+    BackHandler { viewModel.onBackPressed() }
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .background(Color.Transparent)
             .safeContentPadding(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
@@ -34,6 +43,8 @@ fun MainScreen(appState: AppState) {
         )
         else Greeting("foo")
     }
+
+    BackHandler(enabled = false) {}
 }
 
 @Composable
